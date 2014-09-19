1c0e3a899dece992303d7f85ed5452d2b71d8953
roney <roneymonte@gmail.com>
2014-09-18 22:20:18 -0300

    helloWorld inicialmente implementado no CodeWarrior 10.6, agora tambem implementado no KDS 1.1.1, usando a placa Freescale FRDM-KL25Z.

--- teste02/Sources/main.c
+++ teste02/Sources/main.c
@@ -38,46 +38,90 @@
 
 static int i = 0;
 
-void delay (unsigned short int atraso);
+void delay (unsigned int atraso);
 
 
 int main(void)
 {
+		SystemInit();
 
 
-	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
-	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
+		SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
+		PORTB_PCR18 = PORT_PCR_MUX(1);
+		PORTB_PCR19 = PORT_PCR_MUX(1);
+
+		SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
+		PORTD_PCR1 = PORT_PCR_MUX(1);
+
+		// coloca o estado do led em 1
+		GPIOB_PSOR |= (1<<18)|(1<<19);
+		GPIOD_PSOR |= (1<<1);
+
+		//Data Direction para os LEDs
+		GPIOB_PDDR |= (1<<18);	// Configura o bit 18 como output (1) Vermelho
+		GPIOB_PDDR |= (1<<19);	// Configura o bit 19 como output (1) Verde
+		GPIOD_PDDR |= (1<<1);	// Configura o bit 1 como output (1)  Azul
+
+		GPIOB_PCOR = (1<<18);	// liga o led
+		GPIOB_PCOR = (1<<19);	// liga o led
+		GPIOD_PCOR = (1<<1);	// liga o led
+		delay (2000000);delay (2000000);
+		delay (2000000);delay (2000000);
+
+	    GPIOB_PTOR = (1<<18);	// inverte o led
+	    GPIOB_PTOR = (1<<19);	// inverte o led
+	    GPIOD_PTOR = (1<<1);	// inverte o led
+	    delay (250000);
+
+	    GPIOB_PTOR = (1<<18);	// inverte o led
+	    GPIOB_PTOR = (1<<19);	// inverte o led
+	    GPIOD_PTOR = (1<<1);	// inverte o led
+	    delay (250000);
+
+	    GPIOB_PTOR = (1<<18);	// inverte o led
+	    GPIOB_PTOR = (1<<19);	// inverte o led
+	    GPIOD_PTOR = (1<<1);	// inverte o led
+	    delay (250000);
+
+	    GPIOB_PTOR = (1<<18);	// inverte o led
+	    GPIOB_PTOR = (1<<19);	// inverte o led
+	    GPIOD_PTOR = (1<<1);	// inverte o led
+	    delay (250000);
+
+		  for (;;) {
+
+		      GPIOB_PTOR = (1<<18);
+		      GPIOB_PTOR = (1<<19);
+		      GPIOD_PTOR = (1<<1);
+
+		      delay (200000);
+
+		      GPIOB_PCOR = (1<<18);	// liga (PSOR) vermelho
+		      GPIOB_PSOR = (1<<19);	// desliga (PTOR) Verde
+		      GPIOD_PSOR = (1<<1);	// desliga (PTOR) Azul
+
+		      delay (200000);
+
+		      GPIOB_PSOR = (1<<18);	// desliga (PSOR) vermelho
+		      GPIOB_PCOR = (1<<19);	// liga (PTOR) Verde
+		      GPIOD_PSOR = (1<<1);	// desliga (PTOR) Azul
+
+		      delay (200000);
+
+		      GPIOB_PSOR = (1<<18);	// desliga (PSOR) vermelho
+		      GPIOB_PSOR = (1<<19);	// desliga (PTOR) Verde
+		      GPIOD_PCOR = (1<<1);	// liga (PTOR) Azul
+
+		      delay (200000);
+
+		      GPIOB_PTOR = (1<<18);
+		      GPIOB_PTOR = (1<<19);
+		      GPIOD_PTOR = (1<<1);
+
+		      delay (200000);
 
 
-	//Data Direction para os LEDs
-	GPIOB_PDDR |= (1<<18);	// Configura o bit 18 como output (1) Vermelho
-	GPIOB_PDDR |= (1<<19);	// Configura o bit 19 como output (1) Verde
-	GPIOD_PDDR |= (1<<1);	// Configura o bit 1 como output (1)  Azul
-
-    /* This for loop should be replaced. By default this loop allows a single stepping. */
-    for (;;) {
-        i++;
-
-        GPIOB_PSOR = (1<<18);	// liga (PSOR) vermelho
-        GPIOB_PTOR = (1<<19);	// desliga (PTOR) Verde
-        GPIOD_PTOR = (1<<1);	// desliga (PTOR) Azul
-
-        delay (1000000);
-
-        GPIOB_PTOR = (1<<18);	// desliga (PSOR) vermelho
-        GPIOB_PSOR = (1<<19);	// liga (PTOR) Verde
-        GPIOD_PTOR = (1<<1);	// desliga (PTOR) Azul
-
-        delay (1000000);
-
-        GPIOB_PTOR = (1<<18);	// desliga (PSOR) vermelho
-        GPIOB_PTOR = (1<<19);	// desliga (PTOR) Verde
-        GPIOD_PSOR = (1<<1);	// liga (PTOR) Azul
-
-        delay (1000000);
-
-
-    }
+		  }
     /* Never leave main */
     return 0;
 }
@@ -85,8 +129,8 @@ int main(void)
 // EOF
 ////////////////////////////////////////////////////////////////////////////////
 
-void delay (unsigned short int atraso)
+void delay (unsigned  int atraso)
 {
-	unsigned short int contador = 0;
+	unsigned  int contador = 0;
 	for (contador=0;contador<atraso;contador++);
 }
