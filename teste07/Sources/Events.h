3a5d05fd09942cec3ba9179b7d45cd7c67a69aa5
roney <roneymonte@gmail.com>
2014-09-23 12:49:17 -0300

    Ensaio com a plataforma FRDM-K64F e uso do RTOS MQX, como primeiro teste de aprendizado. Sao Paulo.

---
+++ teste07/Sources/Events.h
@@ -0,0 +1,62 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : teste07
+**     Processor   : MK64FN1M0VLL12
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2014-09-23, 12:34, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMI - void Cpu_OnNMI(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "pin_mux.h"
+#include "MQX1.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.4 [05.10]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
