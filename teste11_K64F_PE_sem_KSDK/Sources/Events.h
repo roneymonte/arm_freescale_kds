0b385954be83db3663053f1412e7462356dadc6f
roney <roneymonte@gmail.com>
2014-09-26 19:53:12 -0300

    Projeto 11, usando o PE e sem o KSDK.

---
+++ teste11_K64F_PE_sem_KSDK/Sources/Events.h
@@ -0,0 +1,87 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : teste11_K64F_PE_sem_KSDK
+**     Processor   : MK64FN1M0VLL12
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2014-09-26, 19:37, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMI - void Cpu_OnNMI(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "pin_mux.h"
+#include "LED_VERD.h"
+#include "LEDpin1.h"
+#include "BitIoLdd1.h"
+#include "LED_AZUL.h"
+#include "LEDpin2.h"
+#include "BitIoLdd2.h"
+#include "LED_VERM.h"
+#include "LEDpin3.h"
+#include "BitIoLdd3.h"
+#include "WAIT1.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMI (module Events)
+**
+**     Component   :  Cpu [MK64FN1M0LL12]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMI(void);
+
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.4 [05.10]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
